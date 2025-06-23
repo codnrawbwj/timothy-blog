c5d9e1b56af80046e10242dac81835d7aff2edd4
codnrawbwj <codnrawbwj@gmail.com>
2025-06-22 22:06:59 -0500

    wip: posts page

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -4,7 +4,6 @@ import { getAllPosts, getPostBySlug } from "@/lib/api";
 import { CMS_NAME } from "@/lib/constants";
 import markdownToHtml from "@/lib/markdownToHtml";
 import Container from "@/app/_components/container";
-import Header from "@/app/_components/header";
 import { PostBody } from "@/app/_components/post-body";
 import { PostHeader } from "@/app/_components/post-header";
 
@@ -21,8 +20,7 @@ export default async function Post(props: Params) {
   return (
     <main>
       <Container>
-        <Header />
-        <article className="mb-32">
+        <article className="mb-32 mt-20">
           <PostHeader
             title={post.title}
             coverImage={post.coverImage}
