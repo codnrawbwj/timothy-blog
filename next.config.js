0fffae2e7aa838f1ddb0f35e3e41b7f3cce40321
codnrawbwj <codnrawbwj@gmail.com>
2025-06-22 21:40:12 -0500

    WIP: landing page

---
+++ next.config.js
@@ -0,0 +1,33 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
+  images: {
+    domains: [],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
+
+module.exports = nextConfig;
--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,12 +1,22 @@
 import { CMS_NAME } from "@/lib/constants";
+import Image from "next/image";
 
 export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-6xl font-bold tracking-tighter leading-tight md:pr-8">
-        Timothy Lim
-      </h1>
-      {/* <h4 className="text-center md:text-left text-lg mt-5 md:pl-8"></h4> */}
+      <div className="flex items-center gap-2">
+        <h1 className="text-5xl md:text-6xl font-bold tracking-tighter leading-tight md:pr-8">
+          Timothy Lim
+        </h1>
+        <div className="rounded-[10px] overflow-hidden">
+          <Image
+            src="https://ccvucvagtkhiqhmomcar.supabase.co/storage/v1/object/public/public-assets//myProfile.jpg"
+            alt="profile pic"
+            width={55}
+            height={55}
+          />
+        </div>
+      </div>
     </section>
   );
 }
